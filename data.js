ae0d08d0a0eaa1ffbf41573ca0359a6c5b5373de
CryptoWab <vansh3725793@gmail.com>
2026-06-24 12:55:48 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,4 @@
+/* data.js */
 const animeData = [
     {
         title: "Naruto All Seasons [Dual Audio] Hindi HD",
@@ -6,3 +7,11 @@ const animeData = [
         link: "anime/naruto-all-episodes.html",
         categories: ["anime", "hindi-dubbed"]
     },
+    {
+        title: "Demon Slayer Season 1 [Hindi Dubbed]",
+        searchTag: "demon slayer kimetsu no yaiba hindi dubbed episodes download",
+        poster: "https://images.justwatch.com/poster/26522431/s332/demon-slayer-kimetsu-no-yaiba.webp",
+        link: "anime/demon-slayer-season-1.html",
+        categories: ["anime", "hindi-dubbed"]
+    }
+];
